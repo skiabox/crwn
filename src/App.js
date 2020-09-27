259607f4ad27b6bf4032d4fe186a65b130753b91
Stavros <stavros.kefaleas@gmail.com>
2020-09-27 17:29:38 +0300

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 //Route only passes history, match, location props in one child component
 //so in the first route only HomePage component gets access to these properties
@@ -13,6 +14,7 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
